b872fa97480ed2de295dbeb291105760ded83a4a
Dean Papastrat <dean.g.papastrat@gmail.com>
2014-04-16 13:29:29 -0400

    Add base code for widget

--- functions/widgets.php
+++ functions/widgets.php
@@ -1,2 +1,17 @@
 <?php
+ 
+class hscube_video_player_widget extends WP_Widget () {
+      function hscube_video_player_widget() {
+        $widget_ops = array( 'classname' => 'hscube_video_player_widget', 'description' => __('A widget that displays a High School Cube video player', 'hscube_video_player_widget') );
+        $control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'example-widget' );
+        $this->WP_Widget( 'hscube-video-player-widget', __('High School Cube Video Player', 'hscube_video_player_widget'), $widget_ops, $control_ops );
+    }
+}     // The example widget class 
+function widget($args, $instance) {
+
+}                        // display the widget
+ 
+function update() {}                        // update the widget
+ 
+function form() {}                          // and of course the form for the widget options
 ?>
